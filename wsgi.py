3696a236ccc5471f5e47a98f05837a81dfd97091
John-Thunder <6301947+John-Thunder@users.noreply.github.com>
2018-04-22 22:21:15 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -24,7 +24,7 @@ application = app = bottle.Bottle()
 def Home():
     resp = bottle.response
     qry = bottle.request.query
-    k, timeout = qry.k, int(qry.get('t', '30'))
+    k, timeout = qry.k, int(qry.get('t', 30))
     indexofu=bottle.request.url.index('u=')
     url=bottle.request.url[indexofu+2:]
     if k and k != ALLOW_KEYS:
